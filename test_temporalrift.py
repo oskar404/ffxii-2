b0ebc3e7b1cba8639698d15c47f0aff21734a6bf
Oskar Lönnberg <k.oskar.lonnberg@gmail.com>
2020-11-30 19:57:40 +0200

    blackify unittests
    Run black for unittest file

--- test_temporalrift.py
+++ test_temporalrift.py
@@ -6,7 +6,7 @@ def test_minimal():
     result = search(0, [1])
     print(result)
     assert len(result) == 1
-    assert result[0] == [(0,1)]
+    assert result[0] == [(0, 1)]
 
 
 def test_simple():
@@ -23,7 +23,18 @@ def test_simple():
 def test_complex():
     start = 7
     clock = [4, 1, 4, 3, 1, 4, 2, 3, 5, 2]
-    path = [(7, 3), (0, 4), (4, 1), (5, 4), (9, 2), (1, 1), (2, 4), (6, 2), (8, 5), (3, 3)]
+    path = [
+        (7, 3),
+        (0, 4),
+        (4, 1),
+        (5, 4),
+        (9, 2),
+        (1, 1),
+        (2, 4),
+        (6, 2),
+        (8, 5),
+        (3, 3),
+    ]
     result = search(start, clock)
     print(result)
     print(f"len(result) -> {len(result)}")
